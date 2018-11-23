372b95f35b804aee4a863d06e595cff292cffd46
lijieniu <niulijie19890522@163.com>
2018-11-23 10:10:41 +0800

    自定义组件

---
+++ components/first/index.js
@@ -0,0 +1,14 @@
+Component({
+    properties: {
+        innerText: {
+            type: String,
+            value: 'this is default value'
+        }
+    },
+    data: {
+        someData: {}
+    },
+    methods: {
+        customMethod: function() {}
+    }
+});
